7b6680200098c2758f295566c85ac3065f7c69dc
Felix Dimmerling <ahoi@felixdim.com>
2025-10-30 12:44:58 +0100

    ADJUST panfd.rb

--- Formula/panfd.rb
+++ Formula/panfd.rb
@@ -1,19 +1,10 @@
 class Panfd < Formula
   desc "Shell script to convert MD to PDF using pandoc and custom template"
-  version "0.0.1"
   homepage "https://github.com/quaggaone/felixdim-format-pandoc"
+  version "0.0.1"
   url "https://github.com/quaggaone/felixdim-format-pandoc/archive/refs/tags/v#{version}.tar.gz"
   sha256 "3c4c96dc94f6067283f896392be53a67a719bd860b7b0f9a57b9dbddc5e415e2"
   license ""
   
   depends_on "pandoc" => ">= 3.1.2"
-
-  
-  def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
-  end
 end
-
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://docs.brew.sh/rubydoc/Formula

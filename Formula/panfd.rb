dc9d77eddae060ab82e434c023028f868f2c0fc7
Felix Dimmerling <ahoi@felixdim.com>
2025-10-30 13:21:32 +0100

    ADJUST FORMULA panfd

--- Formula/panfd.rb
+++ Formula/panfd.rb
@@ -1,7 +1,7 @@
 class Panfd < Formula
   desc "Shell script to convert MD to PDF using pandoc and custom template"
   homepage "https://github.com/quaggaone/felixdim-format-pandoc"
-  url "https://github.com/quaggaone/felixdim-format-pandoc/archive/refs/tags/v#{version}.tar.gz"
+  url "https://github.com/quaggaone/felixdim-format-pandoc/archive/refs/tags/v0.0.1.tar.gz"
   version "0.0.1"
   sha256 "3c4c96dc94f6067283f896392be53a67a719bd860b7b0f9a57b9dbddc5e415e2"
   license ""

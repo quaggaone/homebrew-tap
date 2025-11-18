9a5863856a2a4175a2972915a67075ee75be441a
Felix Dimmerling <ahoi@felixdim.com>
2025-11-18 12:14:22 +0100

    UPDATE panfd to v0.1.3

--- Formula/panfd.rb
+++ Formula/panfd.rb
@@ -1,18 +1,18 @@
 class Panfd < Formula
   desc "Shell script to convert MD to PDF using pandoc and custom template"
   homepage "https://github.com/quaggaone/felixdim-format-pandoc"
-  url "https://github.com/quaggaone/felixdim-format-pandoc/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "49f915d187c1a2718e1fc268376d6943ab325df05b5b2291ad8897bc519af47f"
+  url "https://github.com/quaggaone/felixdim-format-pandoc/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "7f8850e9a7a2e182650fffcb3dcc369f513a2e80b6d4e3ffc01258d9c6f134bf"
   license ""
 
   depends_on "pandoc"
   depends_on "typst"
+  depends_on "felixdim-format-typst"
 
   def install
     # install the shell script to homebrew's bin directory
     bin.install "bin/panfd.sh" => "panfd"
 
-    lib.install Dir["lib/*"]
     share.install Dir["share/*"]
   end
 end

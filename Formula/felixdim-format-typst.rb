75fb433cda75a6c100372ed1f8d4834d395a3ff1
Felix Dimmerling <ahoi@felixdim.com>
2025-11-27 23:29:51 +0100

    UPDATE felixdim-format-typst to v0.2.1

--- Formula/felixdim-format-typst.rb
+++ Formula/felixdim-format-typst.rb
@@ -1,8 +1,8 @@
 class FelixdimFormatTypst < Formula
   desc "Collection of personal Typst templates in felixdim style"
   homepage "https://github.com/quaggaone/felixdim-format-typst"
-  url "https://github.com/quaggaone/felixdim-format-typst/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "0012827762e3078e9f0286de333b3f75b440f0adb624e742d7e967480f7bf370"
+  url "https://github.com/quaggaone/felixdim-format-typst/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "07701d482b2eb69335d5e12758696d1f86e762e9f4f001b013243854d23f1e49"
   license ""
 
   depends_on "typst"
